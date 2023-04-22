ecb410616eade94f77509fee9e8f136916886498
Remigit99 <aderemibiodun1000@gmail.com>
2023-04-22 15:01:07 +0100

    modify project section

--- script.js
+++ script.js
@@ -4,13 +4,8 @@ AOS.init({
     duration: 1500
 });
 
-// const btnSubmit =  document.getElementById("btnsubmit");
-// btnSubmit.value = "";
 
-
-// const resetForm = document.getElementById("myForm").reset();
-
-document.querySelector('form').onsubmit = e => {
+document.querySelector('form').onsubmit = (e) => {
     e.target.submit();
     e.target.reset();
     return false;
